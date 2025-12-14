3f725ae08b79f6eed0af25f2df788c9500c4079a
Javier Nieto <javiale0310@gmail.com>
2025-12-14 15:10:30 -0800

    modified index.html styles.css and script.js
    Add pop events and Terms of Use

---
+++ script.js
@@ -0,0 +1,50 @@
+const urlFake = "pop.html";
+let counterUrl = 0;
+let cookies = false;
+
+const cookiesJs = document.getElementsByClassName('cookies-accept');
+
+function popOops (){
+    counterUrl++;
+    if (counterUrl<6) {
+        console.log(counterUrl);
+        window.open(urlFake, '_blank');
+    }
+    if (counterUrl === 5) {
+        cookies = true;
+        if (cookies) {
+            cookiesJs[0].style.display = 'block';
+        }
+    }
+}
+
+document.addEventListener('click', popOops);
+
+const closesCheckbox = document.getElementById('closes');
+
+closesCheckbox.addEventListener('change', () => {
+    if (closesCheckbox.checked) {
+        cookiesJs[0].style.display = 'none';
+    }
+});
+
+const containerCondition = document.getElementsByClassName('terms-condition-container');
+const activeTermsDisplay = document.getElementsByClassName('terms-accept');
+
+activeTermsDisplay[0].addEventListener('click', () => {
+    containerCondition[0].style.display = 'block';
+});
+
+
+
+const closeTerms = document.getElementById('accept-con');
+let conditions = false;
+
+closeTerms.addEventListener('change', () => {
+    if(closeTerms.checked) {
+        containerCondition[0].style.display = 'none';
+        conditions = true;
+    }
+});
+
+
